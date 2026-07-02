0e20478e84beb64e8564c7a5af950c7b6fcb3165
harpalrathod <harpal.rathod88@gmail.com>
2026-07-02 12:42:21 -0500

    Use Customizer business details in footer

--- footer.php
+++ footer.php
@@ -4,6 +4,10 @@
  *
  * @package AP_Luxury
  */
+$ap_phone   = ap_luxury_get_option( 'ap_luxury_phone', '+1 000 000 0000' );
+$ap_address = ap_luxury_get_option( 'ap_luxury_address', 'Rockwall, TX' );
+$ap_hours   = ap_luxury_get_option( 'ap_luxury_hours', 'Monday - Saturday: 10 AM - 7 PM' );
+$tel_link   = preg_replace( '/[^0-9+]/', '', $ap_phone );
 ?>
 </main>
 <footer class="site-footer">
@@ -27,7 +31,7 @@
 		</div>
 		<div>
 			<h2><?php esc_html_e( 'Visit', 'ap-luxury' ); ?></h2>
-			<p>Rockwall, TX<br><a href="tel:+10000000000"><?php esc_html_e( 'Call to Book', 'ap-luxury' ); ?></a></p>
+			<p><?php echo esc_html( $ap_address ); ?><br><a href="tel:<?php echo esc_attr( $tel_link ); ?>"><?php echo esc_html( $ap_phone ); ?></a><br><?php echo esc_html( $ap_hours ); ?></p>
 			<a class="footer-book" href="<?php echo esc_url( ap_luxury_booking_url() ); ?>"><?php esc_html_e( 'Book Appointment', 'ap-luxury' ); ?></a>
 		</div>
 	</div>
